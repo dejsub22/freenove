22358ed60d8149869faed220f38fad844fae5ced
dejsub22 <dej2202@skole.vtfk.no>
2023-12-05 11:10:38 +0100

    asd

---
+++ pico_blink.py
@@ -0,0 +1,13 @@
+from machine import Pin
+import time
+
+led = Pin("LED", Pin.OUT)
+
+try:
+    while True:
+        led.value(1)
+        time.sleep(0.5)
+        led.value(0)
+        time.sleep(0.5)
+except:
+    pass
